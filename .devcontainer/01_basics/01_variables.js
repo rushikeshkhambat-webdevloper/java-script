4cb94f838bf65d683a90f1b3a08885884cec8114
rushikeshkhambat-webdevloper <rushikeshkhambat17@gmail.com>
2026-07-21 14:14:02 +0000

    data types and variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,9 @@
+// we are learning abut key words in the js this key words are used to create a varibales in js 
+const accId = "245678"// const means we cant not change the value in the accId 
+let accState;// if we not assign any value in this it will give us an output of undefined
+let   accHolder = "rushi"// let keyword allows us to modify the value in the variable 
+var   accCity =" csnv "// here var is nothing but keyword which also allow us to make changes in value but it is old way we did not used it now we let instead of var 
+// to print every thin in we will used table 
+accHolder = " abhi"
+arrCity = " bhopal"
+console.table([accId,accCity,accHolder,accState]); // we will get our output in the form of tabel 
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict"// we write this because now we are using new version of js
+//alert(3+3)// it will not give uss 6 because we are using nodejs not browsser it will give us output as a undefinde 
+// data types in js 
+// number when we have number value then we used it 
+let age = 18// ex of number 
+// console.log(typeof 18); it will give us a type of age which is 18 
+// string // used when we have set of characters 
+// bollean // for true and false 
+// bigint// for large numbers 
+// null// // for standalone value 
+// undefinde 
+// symbol// for uniquness or unique
+// above data types are the primitve data types 
+// one more is there 
+
+// object it is an imp we wil se it ahead 
+let name = null
+console.log(typeof null)// it will give us an object 
