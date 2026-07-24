f863c7f1b34a82fc43e2503ca72b332d206fb7ed
rushikeshkhambat-webdevloper <rushikeshkhambat17@gmail.com>
2026-07-24 05:45:52 +0000

    summary of data types and its classification

---
+++ .devcontainer/01_basics/06_dssummary.js
@@ -0,0 +1,18 @@
+// now we have two types of data 
+// primitive : they are pass by value(( pass by value )) in built we can say ex numbers strings undefined symbol 
+// null bigInt bollean 
+// non primitive : the are pass by reference they are made by user and made using primitive 
+//ex Arrays , function , objects 
+// Arrays : grp of varibales or collection of differnt dataty pes 
+const heroes =["rushi"," Abhi"];
+console.log(heroes);// typeof heroes is object 
+// objects: they are ther grp of collection of data of similar thing  return type is object 
+let person={ 
+    name:" rushi",
+    age: 20,
+}
+console.log(" person");
+// functions: it is a peice of code written to perform any task 
+const myfunction= function(){// return type of function is object
+       console.log("hello world");// 
+}
